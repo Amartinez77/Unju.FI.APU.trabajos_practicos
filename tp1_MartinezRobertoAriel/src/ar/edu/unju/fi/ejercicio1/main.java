44893e2ae46ad3f206175cd664cce0fb45b19c0d
Ariel Martinez <ariel.mtz85@gmail.com>
2024-04-10 00:26:24 -0300

    Ejercicio nro1 terminado

--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio1/main.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio1/main.java
@@ -3,7 +3,24 @@ package ar.edu.unju.fi.ejercicio1;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// desarrollo de ejercicio
+		
+		
+		int a, b, c, d, e;
+		
+		double promedio;
+		
+		a=10;
+		b=8;
+		c=3;
+		d=7;
+		e=6;
+		
+		promedio=(a+b+c+d+e)/5;
+		
+		
+		System.out.println("el promedio es: "+promedio);
+		
 
 	}
 
