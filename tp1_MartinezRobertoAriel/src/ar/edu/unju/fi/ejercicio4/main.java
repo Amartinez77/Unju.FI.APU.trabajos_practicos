8baf4508588803bc318596d3c7a8bc1b5557323c
Ariel Martinez <ariel.mtz85@gmail.com>
2024-04-10 12:43:43 -0300

    agregando ejercicio 4 terminado

--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio4/main.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio4/main.java
@@ -1,10 +1,48 @@
 package ar.edu.unju.fi.ejercicio4;
 
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// desarrollo
+		
+		int nro;
+		Scanner scanner = new Scanner(System.in);
+		do {
+			System.out.println("ingrese un nro entre 0 y 10");
+			nro = scanner.nextInt();
+			
+		}while(nro<0 || nro>10);
+		
+		
+		System.out.println("el nro ingresado es: "+nro);
+		
+		int factorial=1;
+		int i = 1;
+		
+		
+		// el if se usa para descartar el 0 ya que por deficinion el factorial es 1
+		if (nro==0) {
+			System.out.println("el factorial del nro "+nro+" es: 1 por definicion");
+
+		}else {
+			
+			while (i<=nro) {
+				
+				factorial= factorial * i;
+				i++;
+				
+			}
+			
+			// se muestra el resultado
+			System.out.println("El factorial de "+nro+" es: "+factorial);
+			
+			scanner.close();
+			
+		}
 
 	}
+	
 
 }
