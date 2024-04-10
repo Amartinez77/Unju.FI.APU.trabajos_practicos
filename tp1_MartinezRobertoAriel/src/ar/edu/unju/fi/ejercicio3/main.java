a03954b7d788e6c5d05b4f824c37d78612487b77
Ariel Martinez <ariel.mtz85@gmail.com>
2024-04-10 11:52:14 -0300

    Subiendo ejercicio 3 terminado

--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio3/main.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio3/main.java
@@ -1,10 +1,27 @@
 package ar.edu.unju.fi.ejercicio3;
 
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// desarrollo
+		
+		int nro;
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("ingrese un numero entero: ");
+		
+		nro=scanner.nextInt();
+		
+		System.out.println("el nro es: "+nro);
+		
+		if (nro % 2 == 0) {
+			
+			System.out.println("el numero es par, su triple es: "+(nro*3));
 
+		}else {
+			System.out.println("el nro es impar, su doble es: "+(nro*2));
+		}
 	}
-
 }
