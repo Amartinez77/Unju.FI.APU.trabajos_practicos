de0d4da28dafd080ed0acfc273b1443a6f628c23
Ariel Martinez <ariel.mtz85@gmail.com>
2024-04-10 01:05:30 -0300

    Creando paquete ejercicio 2 - creando clase main - ejercicio 2 terminado

---
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio2/main.java
@@ -0,0 +1,33 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class main {
+
+	public static void main(String[] args) {
+		// desarrollo
+		
+		
+		String nombrePais; 
+		short edadPersona;
+		float altEdificio;
+		float precioProd;
+		String nroTel;
+		double coseno;
+		
+		nombrePais = "Argentina";
+		edadPersona = 39;
+		altEdificio = 24.3f;
+		precioProd = 33.50f;
+		nroTel = "154999666";
+		coseno = 0.87758256189037;
+		
+		
+		System.out.println("nombre de Pais: "+ nombrePais);
+		System.out.println("edad de una persona: "+ edadPersona);
+		System.out.println("altura de un edificio: "+ altEdificio);
+		System.out.println("precio de un producto: "+ precioProd);
+		System.out.println("nro de tel: "+ nroTel);
+		System.out.println("coseno de 0.5: "+ coseno);
+
+	}
+
+}
