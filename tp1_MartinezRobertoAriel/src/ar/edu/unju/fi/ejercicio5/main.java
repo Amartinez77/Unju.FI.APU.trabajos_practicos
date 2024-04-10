9f3e98ed06ee0fb8d23d2fbb575df5ced438e32f
Ariel Martinez <ariel.mtz85@gmail.com>
2024-04-10 13:02:14 -0300

    agreganto ejercicio 5 terminado

--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio5/main.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio5/main.java
@@ -1,9 +1,31 @@
 package ar.edu.unju.fi.ejercicio5;
 
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// desarrollo
+		
+		int nro;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("ingrese un numero entre 1 y 9");
+		
+		nro = scanner.nextInt();
+		
+		if(nro >= 1 || nro <= 9) {
+			
+			for (int i = 0 ; i <= 10; i++) {
+				
+				System.out.println(nro+" x "+i+" = "+(nro*i));
+				
+			}
+			
+		}else {
+			System.out.println("recuerde que debe ingresar un numero entre 1 y 9");
+		}
+		
+		scanner.close();
 
 	}
 
