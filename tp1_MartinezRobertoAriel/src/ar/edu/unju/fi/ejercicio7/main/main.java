a2e15de10c26c7af3c6f1f451dfd3efb1c4a397c
Ariel Martinez <ariel.mtz85@gmail.com>
2024-04-11 00:01:38 -0300

    subiendo ejercicio 7 terminado

--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio7/main/main.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio7/main/main.java
@@ -1,9 +1,37 @@
 package ar.edu.unju.fi.ejercicio7.main;
 
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio7.model.Empleado;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//desarrollo
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("ingrese los datos de empleado/n");
+		
+		System.out.print("Nombre del empleado: ");		
+		String nombre = scanner.next();
+		System.out.print("Ingrese legajo: ");		
+		Integer legajo = scanner.nextInt();
+		System.out.print("Ingrese el salario: ");		
+		Double salario = scanner.nextDouble();
+		
+		Empleado empleado = new Empleado(nombre, legajo, salario);
+		
+		empleado.mostrarDatos();
+		
+		System.out.println("/n");
+		
+		System.out.println("realizando aumento de salario");
+		System.out.println("/n");
+		
+		empleado.aumentoSalario();
+		
+		empleado.mostrarDatos();
 
 	}
 
--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio7/model/Empleado.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio7/model/Empleado.java
@@ -1,5 +1,41 @@
 package ar.edu.unju.fi.ejercicio7.model;
 
 public class Empleado {
+	
+	private String nombre;
+	private Integer legajo;
+	private Double salario;
 
+	public static final Double SALARIO_MINIMO = 210000.00;
+	public static final Double AUMENTO_MERITO = 20000.00;
+	
+	//constructores
+	public Empleado() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Empleado(String nombre, Integer legajo, Double salario) {
+		super();
+		this.nombre = nombre;
+		this.legajo = legajo;
+		this.salario = Math.max(salario, SALARIO_MINIMO);		
+		
+	}
+	
+	//procedimiento de muestra de datos
+	public void mostrarDatos() {
+		
+		System.out.println("Nombre del empleado: "+nombre);
+		System.out.println("Legajo: "+legajo);
+		System.out.println("Salario: "+salario);
+		
+	}
+	
+	// procedimiento que realiza un aumento de salario aumentando al salario una constante aumento_merito
+	public void aumentoSalario() {
+		this.salario = salario+AUMENTO_MERITO;
+		
+	}
+	
 }
