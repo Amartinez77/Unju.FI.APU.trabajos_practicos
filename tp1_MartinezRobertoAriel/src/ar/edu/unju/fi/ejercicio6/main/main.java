2d32fd5d073d5e0b462fae85b863c1ed96a369ed
Ariel Martinez <ariel.mtz85@gmail.com>
2024-04-10 22:16:49 -0300

    agregando ejercicio 6 terminado

--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio6/main/main.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio6/main/main.java
@@ -1,10 +1,130 @@
 package ar.edu.unju.fi.ejercicio6.main;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio6.model.Persona;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//desarrollo
+		
+		
+		//persona creada con el constructor por defecto
+		Persona persona = generarPersona();
+		
+		persona.mostrarDatos();
+		
+		System.out.println("\n");
+		
+		//persona creada con el constructor parametrizado
+		generarPersonaParametrizada();
+		
+		System.out.println("\n");
+		
+		//llamada al procedimiento que carga e instancia una persona que tiene un parametro menos ya que se carga dentro del constructor
+		generarPersona3();
+		
+		
+		
+		
 
 	}
-
+	
+	// metodo para poder utilizar el constructor por defecto
+	
+	public static Persona generarPersona() {
+		
+		Persona persona = new Persona();
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("ingrese el DNI: ");
+		String dni = scanner.next();
+		System.out.println("ingrese el nombre: ");
+		String nombre = scanner.next();
+		System.out.println("ingrese la fecha de nacimiento en formato dd/mm/aaaa: ");
+		String fecha = scanner.next();
+		System.out.println("ingrese la provincia: ");
+		String provincia = scanner.next();
+		
+		persona.setDni(dni);
+		persona.setNombre(nombre);
+		
+		//se crea el formato de la fecha, luego se crea un localdate y se le asigna la fecha(string) y se la formatea
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		LocalDate fecnac = LocalDate.parse(fecha, formatter);
+		
+		persona.setFecNac(fecnac);
+		persona.setProvincia(provincia);
+		
+		
+		
+		return persona;
+			
+	}
+	
+	
+	//procedimiento que genera una persona en el contructor sobrecargado
+	public static void generarPersonaParametrizada() {
+		
+				
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("ingrese el DNI: ");
+		String dni = scanner.next();
+		System.out.println("ingrese el nombre: ");
+		String nombre = scanner.next();
+		System.out.println("ingrese la fecha de nacimiento en formato dd/mm/aaaa: ");
+		String fecha = scanner.next();
+		System.out.println("ingrese la provincia: ");
+		String provincia = scanner.next();
+		
+		
+		
+		//se crea el formato de la fecha, luego se crea un localdate y se le asigna la fecha(string) y se la formatea
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		LocalDate fecnac = LocalDate.parse(fecha, formatter);
+		
+		Persona persona2 = new Persona(dni, nombre, fecnac, provincia);
+		
+		persona2.mostrarDatos();
+		
+		
+		
+		
+	}
+	
+	
+	public static void generarPersona3() {
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("ingrese el DNI: ");
+		String dni = scanner.next();
+		System.out.println("ingrese el nombre: ");
+		String nombre = scanner.next();
+		System.out.println("ingrese la fecha de nacimiento en formato dd/mm/aaaa: ");
+		String fecha = scanner.next();
+				
+		
+		
+		//se crea el formato de la fecha, luego se crea un localdate y se le asigna la fecha(string) y se la formatea
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		LocalDate fecnac = LocalDate.parse(fecha, formatter);
+		
+		Persona persona3 = new Persona(dni, nombre, fecnac);
+		
+		persona3.mostrarDatos();
+		
+		
+		
+		
+	}
+	
+	
+		
 }
--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio6/model/Persona.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio6/model/Persona.java
@@ -1,5 +1,100 @@
 package ar.edu.unju.fi.ejercicio6.model;
 
-public class Persona {
+import java.time.LocalDate;
+import java.time.Period;
 
+public class Persona {
+	
+	private String dni;
+	private String nombre;
+	private LocalDate fecNac;
+	private String provincia;
+	
+	// constructores	
+	public Persona() {
+		// TODO Auto-generated constructor stub
+	}
+			
+	public Persona(String dni, String nombre, LocalDate fecNac, String provincia) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.fecNac = fecNac;
+		this.provincia = provincia;
+	}
+
+	
+	public Persona (String dni, String nombre, LocalDate fecNac) {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.fecNac = fecNac;
+		this.provincia = "Jujuy";
+		
+	}
+
+	// metodos accesores
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public LocalDate getFecNac() {
+		return fecNac;
+	}
+	public void setFecNac(LocalDate fecNac) {
+		this.fecNac = fecNac;
+	}
+	public String getProvincia() {
+		return provincia;
+	}
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+	
+	// metodo que calcula la edad de una persona en base a su fecha de nacimiento, usando localdate	
+	public int calcularEdad() {
+		
+		LocalDate fechaHoy = LocalDate.now();
+		
+		Period periodo = Period.between(fecNac, fechaHoy);
+		
+		int edad = periodo.getYears();
+		
+		return edad;
+		
+	}
+	
+	public boolean verificarEdad() {
+		
+		int edad = calcularEdad();
+		
+		return edad>18;
+		
+	}
+	
+	public void mostrarDatos() {
+		
+		int edad=calcularEdad();
+		System.out.println("Nombre: "+nombre);
+		System.out.println("Dni: "+dni);
+		System.out.println("fecha de nacimiento: "+fecNac);
+		System.out.println("Provincia: "+provincia);
+		System.out.println("Edad: "+edad);
+	
+		if (edad > 18) {
+			System.out.println("La persona es mayor de edad");
+		}else {
+			System.out.println("La persona no es mayor de edad");
+		}
+	
+	
+
+}
 }
