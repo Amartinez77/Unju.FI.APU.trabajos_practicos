382907f8dc99e3d6c05d0d15c9de622342687e1d
Ariel Martinez <ariel.mtz85@gmail.com>
2024-04-12 01:04:19 -0300

    Agregando ejercicio 8 terminado

--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio8/main/Main.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio8/main/Main.java
@@ -1,9 +1,30 @@
 package ar.edu.unju.fi.ejercicio8.main;
 
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio8.model.CalculadoraEspecial;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//desarrollo
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("ingrese un numero entero");
+		int nro = scanner.nextInt();
+		
+		CalculadoraEspecial calculadora = new CalculadoraEspecial();
+		calculadora.setN(nro);
+		
+		double sum= calculadora.calcularSumatoria();
+		int prod= calculadora.calcularProductoria();
+
+		
+		
+		System.out.println("el resultado de la sumatoria de "+nro +" es: "+sum);
+		System.out.println("el resultado de la productoria de "+nro +" es: "+prod);
+		
 
 	}
 
--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio8/model/CalculadoraEspecial.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio8/model/CalculadoraEspecial.java
@@ -1,5 +1,57 @@
 package ar.edu.unju.fi.ejercicio8.model;
 
+
 public class CalculadoraEspecial {
+	
+	private int n;
+	
+	public CalculadoraEspecial() {
+		// constructor por defecto
+	}
+
+	
+	
+	public int getN() {
+		return n;
+	}
+
+
+
+	public void setN(int n) {
+		this.n = n;
+	}
+
+
+
+	public double calcularSumatoria() {
+		double sumatoria = 0;
+		double result;
+		
+		for (int k = 1; k <= n; k++) {
+			
+			result = ((k*(k+1))/2) * ((k*(k+1))/2);
+			sumatoria = sumatoria + result;
+			
+		}
+		
+		return sumatoria;
+	}
+	
+	
+	public int calcularProductoria() {
+		
+		int productoria = 1;
+		int result;
+		
+		for (int k = 1; k <= n; k++) {
+			
+			result = (k*(k+4));
+			productoria = productoria * result;
+			
+		}
+		
+		return productoria;
+		
+	}
 
 }
