73c1d641fc183c3e5cc78122b73011165127eaa3
Ariel Martinez <ariel.mtz85@gmail.com>
2024-04-12 21:13:37 -0300

    agregando ejercicio 9 terminado

--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio9/main/Main.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio9/main/Main.java
@@ -1,10 +1,64 @@
 package ar.edu.unju.fi.ejercicio9.main;
 
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio9.model.Producto;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// desarrollo
+		
+		
+		Scanner scanner = new Scanner(System.in); 
+		
+		for (int i = 1; i <= 3; i++) {
+			
+			System.out.println("ingrese el nombre de producto");
+			
+			String nombreProducto = scanner.next();
+			
+			Producto producto = new Producto();
+			
+			producto.setNombre(nombreProducto);
+			
+			System.out.println("Ingrese el codigo de producto:");
+			Integer cod = scanner.nextInt();
+			
+			System.out.println("Ingrese el precio del producto:");
+			Double precio = scanner.nextDouble();
+			
+			Integer descuento=0;
+			
+			do {
+				
+				System.out.println("Ingrese el descuento 0 a 50 :");
+				
+				descuento = scanner.nextInt();
+				
+			} while (descuento < 0 || descuento > 50);
+			
+			producto.setCodigo(cod);
+			producto.setPrecio(precio);
+			producto.setDescuento(descuento);
+			
+			
+			mostrarProductos(producto, i);
+			
+		}
 
+	}
+	
+	public static void mostrarProductos(Producto producto, int numeroProducto) {
+		
+		System.out.println("Producto " + numeroProducto + ":");
+        System.out.println("Nombre: " + producto.getNombre());
+        System.out.println("Código: " + producto.getCodigo());
+        System.out.println("Precio: " + producto.getPrecio());
+        System.out.println("Descuento: " + producto.getDescuento());
+        System.out.println("Precio final con descuento: " + producto.calcularDescuento());
+        System.out.println();
+		
 	}
 
 }
--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio9/model/Producto.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio9/model/Producto.java
@@ -1,5 +1,57 @@
 package ar.edu.unju.fi.ejercicio9.model;
 
 public class Producto {
+	
+	private String nombre;
+	private Integer codigo;
+	private Double precio;
+	private Integer descuento;
+	
+	public Producto() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public Integer getDescuento() {
+		return descuento;
+	}
+
+	public void setDescuento(Integer descuento) {
+		this.descuento = descuento;
+	}
+	
+	
+	public Double calcularDescuento() {
+		
+		Double precioFinal;
+		
+		precioFinal = precio-descuento;
+		
+		return precioFinal;
+		
+	}
 
 }
