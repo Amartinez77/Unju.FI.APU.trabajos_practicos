8aacdce899c5964a6b984f6fa9b8f28755cccf6a
Ariel Martinez <ariel.mtz85@gmail.com>
2024-04-12 22:17:30 -0300

    agregando ejercicio 10 terminado

--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio10/main/Main.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio10/main/Main.java
@@ -1,9 +1,45 @@
 package ar.edu.unju.fi.ejercicio10.main;
 
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio10.model.Pizza;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// desarrollo
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		 // Bucle para crear y solicitar datos para 3 objetos Pizza
+        for (int i = 1; i <= 3; i++) {
+            Pizza pizza = new Pizza();
+
+            // Solicitar diámetro
+            System.out.println("Ingrese el diámetro de la pizza " + i + " (20, 30 o 40):");
+            int diametro = Integer.parseInt(scanner.nextLine());
+            pizza.setDiametro(diametro);
+
+            // Solicitar si lleva ingredientes especiales
+            System.out.println("¿Lleva ingredientes especiales? (true/false):");
+            boolean ingredientesEspeciales = Boolean.parseBoolean(scanner.nextLine());
+            pizza.setIngredientesEspeciales(ingredientesEspeciales);
+
+            // Calcular precio y área
+            pizza.calcularPrecio();
+            pizza.calcularArea();
+
+            // Mostrar los datos de la pizza
+            System.out.println("** Pizza " + i + " **");
+            System.out.println("Diámetro = " + pizza.getDiametro());
+            System.out.println("Ingredientes especiales = " + pizza.isIngredientesEspeciales());
+            System.out.println("Precio Pizza = " + pizza.getPrecio() + "$");
+            System.out.println("Área de la pizza = " + pizza.getArea());
+            System.out.println();
+            
+        }
+
+        scanner.close();
 
 	}
 
--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio10/model/Pizza.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio10/model/Pizza.java
@@ -1,5 +1,88 @@
 package ar.edu.unju.fi.ejercicio10.model;
 
 public class Pizza {
+	
+	private int diametro;
+	private double precio;
+	private double area;
+	private boolean ingredientesEspeciales;
+	
+	final int ADICIONAL20 = 500;
+	final int ADICIONAL30 = 750;
+	final int ADICIONAL40 = 1000;
+	
+	public Pizza() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public int getDiametro() {
+		return diametro;
+	}
+
+	public void setDiametro(int diametro) {
+		this.diametro = diametro;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public double getArea() {
+		return area;
+	}
+
+	public void setArea(double area) {
+		this.area = area;
+	}
+
+	public boolean isIngredientesEspeciales() {
+		return ingredientesEspeciales;
+	}
+
+	public void setIngredientesEspeciales(boolean ingredientesEspeciales) {
+		this.ingredientesEspeciales = ingredientesEspeciales;
+	}
+
+	public int getADICIONAL20() {
+		return ADICIONAL20;
+	}
+
+	public int getADICIONAL30() {
+		return ADICIONAL30;
+	}
+
+	public int getADICIONAL40() {
+		return ADICIONAL40;
+	}
+
+
+	// Método para calcular el precio de la pizza
+    public void calcularPrecio() {
+        if (diametro == 20) {
+        	
+            precio = (ingredientesEspeciales ? 4500 + ADICIONAL20 : 4500);
+            
+        } else if (diametro == 30) {
+        	
+            precio = ingredientesEspeciales ? 4800 + ADICIONAL30 : 4800;
+            
+        } else if (diametro == 40) {
+        	
+            precio = ingredientesEspeciales ? 5500 + ADICIONAL40 : 5500;
+            
+        }
+    }
+
+    // Método para calcular el área de la pizza
+    public void calcularArea() {
+    	
+        area = Math.PI * Math.pow(diametro / 2.0, 2);
+        
+    }
 
 }
