ff7405fa01921665dad18a0563f02ea22985d9bc
Ariel Martinez <ariel.mtz85@gmail.com>
2024-04-16 19:02:42 -0300

    agregando ejercicio 14 terminado

--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio14/Main.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio14/Main.java
@@ -1,13 +1,66 @@
 package ar.edu.unju.fi.ejercicio14;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		//desarrollo
 		
 		
+		Scanner scanner = new Scanner(System.in);
 		
-
+		System.err.println("ingrese un nro dentro de este rango [3, 10]");
+		
+		int nro = scanner.nextInt();
+		
+		
+		int[] arreglo = new int[nro];
+		
+		
+		for(int i = 0; i < arreglo.length; i++){
+			
+			System.out.println("ingrese un nro para almacenarlo en el array");
+			
+			arreglo[i] = scanner.nextInt();
+			
+						
+		}	
+		
+		System.out.println("-------------------------");
+		mostrar(arreglo);
 	}
-
+	
+	/*
+	 * funcion que recibe un arregle de n elementos y devuelve la suma de los elementos
+	 */
+	public static int suma(int[] arreglo) {
+		
+		int suma = 0;
+		
+		for(int i = 0; i < arreglo.length; i++){
+			
+			suma = suma + arreglo[i];
+		
+		}
+	
+		return suma;
+	}
+	
+	// procedimiento de muestra
+	public static void mostrar(int[] arreglo) {
+		
+		int suma = 0;
+		
+		for(int i = 0; i < arreglo.length; i++){
+			
+			System.out.println("posicion: " + i + " valor: " + arreglo[i]);
+		
+		}
+		
+		System.out.println("la suma total de elementos es: "+ suma(arreglo));
+	
+		
+	}
+	
 }
