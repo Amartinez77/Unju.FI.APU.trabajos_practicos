e3b752b5d43a5547b633975cf25d6878f643059c
Ariel Martinez <ariel.mtz85@gmail.com>
2024-04-16 17:05:57 -0300

    Agregando ejercicio 12 terminado

--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio12/main/Main.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio12/main/Main.java
@@ -1,10 +1,70 @@
 package ar.edu.unju.fi.ejercicio12.main;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio12.model.Persona;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// desarrollo
+		
+		
+		
+		Scanner scanner = new Scanner(System.in);
+				
+		System.out.println("ingrese el nombre de la persona");
+		
+		String nombre = scanner.nextLine(); 
+		
+		System.out.println("ingrese suy fecha de nacimiento");
+		
+		String fechaIngresada = scanner.nextLine();
+		
+		
+		Calendar fecNac = convertirFecha(fechaIngresada);
+		
+		
+		
+		Persona persona = new Persona(nombre, fecNac);
+		
+		
+		System.out.println("la edad es: "+persona.edad());
+		
+		System.out.println("el signo es: "+persona.signo());
+		
+		System.out.println("Estación en la que nació: "+persona.estacion());
+		
+		
+		
+		
 	}
+	
+	/*
+	* funcion que recibe una fecha en formato string dd/mm/aaaa y devuelve la fecha en formato calendar
+	* se utiliza un bloque try catch por recomendacion del IDE para manejar el error que podria ocurrir
+	* al parsear la fecha
+	*/
+	public static Calendar convertirFecha (String fechaIngresada) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Calendar calendar = Calendar.getInstance();
+		
+		
+		try {
+			calendar.setTime(sdf.parse(fechaIngresada));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+		
+		return calendar;
+		
+	}
+	
 
 }
--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio12/model/Persona.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio12/model/Persona.java
@@ -1,6 +1,139 @@
 package ar.edu.unju.fi.ejercicio12.model;
 
+import java.util.Calendar;
+
 public class Persona {
+	
+	private String nombre;
+	private Calendar fecNac;
+	
+	
+	public Persona() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Persona(String nombre, Calendar fecNac) {
+		super();
+		this.nombre = nombre;
+		this.fecNac = fecNac;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public Calendar getFecNac() {
+		return fecNac;
+	}
+
+
+	public void setFecNac(Calendar fecNac) {
+		this.fecNac = fecNac;
+	}
+	
+	
+	public int edad() {
+		
+		Calendar hoy = Calendar.getInstance();
+		int edad = hoy.get(Calendar.YEAR) - fecNac.get(Calendar.YEAR);
+		int mesHoy = hoy.get(Calendar.MONTH);
+	    int diaHoy = hoy.get(Calendar.DAY_OF_MONTH);
+	    int mesNacimiento = fecNac.get(Calendar.MONTH);
+	    int diaNacimiento = fecNac.get(Calendar.DAY_OF_MONTH);
+		
+	    if (mesHoy < mesNacimiento || (mesHoy == mesNacimiento && diaHoy < diaNacimiento)) {
+	        edad--;
+	    }
+		
+		return edad;
+	}
+	
+	public String signo() {
+		
+		int mes = fecNac.get(Calendar.MONTH) +1; //obtego el mes pero como va de 0 a 11 le sumo 1, asi funciona calendar
+		int dia = fecNac.get(Calendar.DAY_OF_MONTH);
+		
+		switch (mes) {
+		case 1: // Enero
+			return (dia <= 20) ? "Capricornio" : "Acuario";
+		case 2: // Febrero
+            return (dia <= 19) ? "Acuario" : "Piscis";
+        case 3: // Marzo
+            return (dia <= 20) ? "Piscis" : "Aries";
+        case 4: // Abril
+            return (dia <= 20) ? "Aries" : "Tauro";
+        case 5: // Mayo
+            return (dia <= 21) ? "Tauro" : "Géminis";
+        case 6: // Junio
+            return (dia <= 21) ? "Géminis" : "Cáncer";
+        case 7: // Julio
+            return (dia <= 22) ? "Cáncer" : "Leo";
+        case 8: // Agosto
+            return (dia <= 23) ? "Leo" : "Virgo";
+        case 9: // Septiembre
+            return (dia <= 23) ? "Virgo" : "Libra";
+        case 10: // Octubre
+            return (dia <= 23) ? "Libra" : "Escorpio";
+        case 11: // Noviembre
+            return (dia <= 22) ? "Escorpio" : "Sagitario";
+        case 12: // Diciembre
+            return (dia <= 21) ? "Sagitario" : "Capricornio";	
+			
+		
+		default:
+			return "signo no válido";
+		}
+		
+		
+	}
+	
+	
+	public String estacion() {
+		
+		int mes = fecNac.get(Calendar.MONTH) +1; //obtego el mes pero como va de 0 a 11 le sumo 1, asi funciona calendar
+		int dia = fecNac.get(Calendar.DAY_OF_MONTH);
+		
+		switch (mes) {
+		case 1: // Enero
+			return "Verano";
+		case 2: // Febrero
+            return "Verano";
+        case 3: // Marzo
+            return (dia <= 20) ? "Verano" : "Otoño";
+        case 4: // Abril
+            return "Otoño";
+        case 5: // Mayo
+            return "Otoño";
+        case 6: // Junio
+            return (dia <= 21) ? "Otoño" : "Invierno";
+        case 7: // Julio
+            return "Invierno";
+        case 8: // Agosto
+            return "Invierno";
+        case 9: // Septiembre
+            return (dia < 21) ? "Invierno" : "Primavera";
+        case 10: // Octubre
+            return "Primavera";
+        case 11: // Noviembre
+            return "Primavera";
+        case 12: // Diciembre
+            return (dia < 21) ? "Primavera" : "Verano";	
+			
+		
+		default:
+			return "signo no válido";
+		
+		}
+		
+	}
 	
 	
 
