9e8558b3e47b4fd33d9ad30884b40645e77385f4
Ariel Martinez <ariel.mtz85@gmail.com>
2024-04-16 18:40:22 -0300

    agregando ejercicio 13 terminado

--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio13/Main.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio13/Main.java
@@ -1,10 +1,47 @@
 package ar.edu.unju.fi.ejercicio13;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int[] numeros = new int[8];
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		/*
+		* se utiliza un ciclo for para cargar y mostrar el array
+		*/
+		
+		for(int i = 0; i < numeros.length; i++) {
+			
+			System.out.println("ingrese un nro");
+			numeros[i] = scanner.nextInt();
+			System.out.println();
+					
+			
+		}	
+		
+		
+		mostrarArray(numeros);
+		
+		
+	}
+	
+	
+	public static void mostrarArray(int[] array) {
+		
+		for(int i = 0; i< array.length; i++) {
+			
+			System.out.println("posición: "+ i +" valor: "+array[i]);
+			
+		}
+		
+		
 	}
 
+
 }
+	
