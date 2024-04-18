6361c53f5def54a5e78bdb3ece5efe5be2237ab5
Ariel Martinez <ariel.mtz85@gmail.com>
2024-04-18 01:20:49 -0300

    agregando ejercicico 17 - falta terminar

--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio17/main/Main.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio17/main/Main.java
@@ -1,10 +1,60 @@
 package ar.edu.unju.fi.ejercicio17.main;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// desarrollo
+		
+		int opc;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		
+		do {
+			
+			System.out.println("*************************************************************");
+			System.out.println("*** 1- Alta de jugador  *************************************");
+			System.out.println("*** 2- Mostrar datos del jugador ****************************");
+			System.out.println("*** 3- Mostrar todos los jugadores ordenados por apellido  **");
+			System.out.println("*** 4- Modificar los datos de un jugador   ******************");
+			System.out.println("*** 5- Eliminar un Jugador  *********************************");
+			System.out.println("*** 6- Cantidad total de jugadores  *************************");
+			System.out.println("*** 7- Mostrar Cantidad de jugadores por nacionalidad   *****");
+			System.out.println("*** 8- Salir   **********************************************");
+			System.out.println("**************************************************************");
+			
+			opc = scanner.nextInt();
+			
+			switch (opc) {
+			
+			case 1: AltaJugador(); break;
+				
+			case 8: System.out.println("Fin del programa");	
+			default:
+				System.out.println("opcion incorrecta");
+			}
+			
+			
+		} while (opc!=8);
+		
+		
+		
 
 	}
+	
+	
+	public static void AltaJugador() {
+		
+		
+		
+		
+	}
+	
+	
+	
+	
 
 }
--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio17/model/Jugador.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio17/model/Jugador.java
@@ -1,5 +1,97 @@
 package ar.edu.unju.fi.ejercicio17.model;
 
+import java.time.LocalDate;
+import java.time.Period;
+
+
+
 public class Jugador {
+	
+	private String nombre;
+	private String apellido;
+	private LocalDate fechaNacimiento;
+	private String nacionalidad;
+	private Double peso;
+	private String posicion;
+	
+	public Jugador() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Jugador(String nombre, String apellido, LocalDate fechaNacimiento, String nacionalidad, Double peso,
+			String posicion) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.fechaNacimiento = fechaNacimiento;
+		this.nacionalidad = nacionalidad;
+		this.peso = peso;
+		this.posicion = posicion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+	public String getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(String posicion) {
+		this.posicion = posicion;
+	}
+	
+	
+	public int calcularEdad() {
+		
+		LocalDate fechaHoy = LocalDate.now();
+		
+		Period period = Period.between(fechaNacimiento, fechaHoy);
+		
+		int edad = period.getYears();
+		
+		return edad;		
+		
+		
+	}
+	
+	
+	
 
 }
