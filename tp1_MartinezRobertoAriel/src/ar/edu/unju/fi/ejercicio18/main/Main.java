17683e9df6ae935d5ddd84fed9165a4ba348662f
Ariel Martinez <ariel.mtz85@gmail.com>
2024-04-20 01:48:13 -0300

    agregando ejercicio 18 - falta terminar

--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio18/main/Main.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio18/main/Main.java
@@ -1,10 +1,104 @@
 package ar.edu.unju.fi.ejercicio18.main;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio18.model.DestinoTuristico;
+import ar.edu.unju.fi.ejercicio18.model.Pais;
+
+
 public class Main {
+	
+	static Scanner scanner;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// desarollo
+		
+		ArrayList<DestinoTuristico> destinos = new ArrayList<>();
+		ArrayList<Pais> listaPaises = new ArrayList<>();
+		
+		
+		listaPaises.add(new Pais(+54, "Argentina"));
+		
+		
+		
+		
+		
+		
+		int opc=0;
+		
+		scanner = new Scanner(System.in);
+		
+		
+		
+		do {
+			
+			System.out.println("*************************************************************");
+			System.out.println("*** 1- Alta de destino turistico  *************************************");
+			System.out.println("*** 2- Mostrar todos los destinos turisticos ****************************");
+			System.out.println("*** 3- Modificar el pais de un destino turistico  **");
+			System.out.println("*** 4- Limpiar el arrayList   ******************");
+			System.out.println("*** 5- Eliminar un destino turistico  *********************************");
+			System.out.println("*** 6- Mostrar los destinos turisticos ordenados por nombre  *************************");
+			System.out.println("*** 7- Mostrar todos los paises   *****");
+			System.out.println("*** 8- Mostrar los destinos turisticos que pertenecen a un pais   **********************************************");
+			System.out.println("*** 9- salir   **********************************************");
+
+			System.out.println("**************************************************************");
+			
+			
+			
+			
+			try {
+				
+				opc = scanner.nextInt();
+				scanner.nextLine();
+				
+			} catch (InputMismatchException e) {
+				
+				System.out.println("debe ingresar un numero !!!!");
+				scanner.nextLine();
+				continue;
+				
+			}		
+			
+			switch (opc) {
+			
+			case 1: 
+					break;
+			
+			//case 2: Mostrar(listaJugadores, true); break;
+			
+			//case 3: Mostrar(listaJugadores, false); break;
+			
+			//case 4: 
+					
+				
+					
+			//case 5: eliminar(listaJugadores);
+					
+					
+			//case 6: cantidad(listaJugadores);
+					
+					
+			//case 7: contadorNacionalidad(listaJugadores);
+					
+				
+			case 9: System.out.println("Fin del programa");	
+					break;
+			
+			default:
+				System.out.println("opcion no valida, intente nuevamente");
+			}
+			
+			
+		} while (opc!=8);
+		
+		
+		
 
 	}
+	
 
 }
--- tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio18/model/DestinoTuristico.java
+++ tp1_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio18/model/DestinoTuristico.java
@@ -1,5 +1,78 @@
 package ar.edu.unju.fi.ejercicio18.model;
 
 public class DestinoTuristico {
+	
+	int codigo ;
+	String nombre;
+	Double precio;
+	int cantidadDias;
+	Pais pais;
+	
+	
+	public DestinoTuristico() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public DestinoTuristico(int codigo, String nombre, Double precio, int cantidadDias, Pais pais) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.cantidadDias = cantidadDias;
+		this.pais = pais;
+	}
+
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+
+	public int getCantidadDias() {
+		return cantidadDias;
+	}
+
+
+	public void setCantidadDias(int cantidadDias) {
+		this.cantidadDias = cantidadDias;
+	}
+
+
+	public Pais getPais() {
+		return pais;
+	}
+
+
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+	
+	
 
 }
